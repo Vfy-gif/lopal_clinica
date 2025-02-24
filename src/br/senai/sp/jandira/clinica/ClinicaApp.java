92fdee9cd016ebf15252ca5278a39d3bead22372
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-02-24 17:00:10 -0300

    Começamos os getters e setters

--- src/br/senai/sp/jandira/clinica/ClinicaApp.java
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -10,23 +10,27 @@ public class ClinicaApp {
 		
 		// Criar um objeto do tipo Paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Ana Maria";
+		p1.setNome("ana maria");
 		p1.cpf = "999.888.666-77";
-		p1.altura = 1.63;
-		p1.peso = 75.490;
+		p1.setAltura(1.63);
+		p1.setPeso(75.490);
 		p1.telefone = "(11)96523-8888";
 		p1.dataNascimento = LocalDate.of(2003, 8, 16);
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "Vitor Miguel";
+		p2.setNome("vitor miguel");
 		p2.cpf = "999.888.666-77";
-		p2.altura = 1.70;
-		p2.peso = 72.000;
+		p2.setAltura(1.72);
+		p2.setPeso(72);
 		p2.telefone = "(11)99367-9608";
 		p2.dataNascimento = LocalDate.of(2009, 2, 7);
 		
 		p1.mostrarDados();
 		p2.mostrarDados();
+		
+		String teste = p1.getNome();
+		System.out.println(teste.toLowerCase());
+		System.out.println(p2.getNome());
 		
 	}
 
